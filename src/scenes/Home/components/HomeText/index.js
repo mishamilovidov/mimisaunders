6cc3a3d13ca87bd6acbf11b683f041199caa5214
Misha Milovidov <m.alexeyvich@gmail.com>
2019-06-24 19:33:28 -0700

    Remove emoji from homepage

--- src/scenes/Home/components/HomeText/index.js
+++ src/scenes/Home/components/HomeText/index.js
@@ -16,7 +16,7 @@ class HomeText extends Component {
       <ReactCSSTransitionGroup {...transitionOptions}>
         <div className="HomeText">
           <div className="title">Mimi Saunders</div>
-          <div className="subtitle">Website Coming Soon <span role="img" aria-label="Woman Technologist">👩‍💻</span></div>
+          <div className="subtitle">Website Coming Soon</div>
         </div>
       </ReactCSSTransitionGroup>
     );
